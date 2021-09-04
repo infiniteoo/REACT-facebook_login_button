88aea3aa090eb00a2e97330190222cb118d0e8ca
Troy Dorman <troydorman@gmail.com>
2021-09-03 20:18:20 -0600

    added a default state to our class based component

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
 
 export default class Facebook extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: "",
+  };
+  render() {
+    return <div></div>;
+  }
 }
