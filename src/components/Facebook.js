7ce359ffa887fca15cf4f4c7d70650b62ee4a2e7
Troy Dorman <troydorman@gmail.com>
2021-09-03 20:24:19 -0600

    added facebook app id to .env file

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -10,6 +10,20 @@ export default class Facebook extends Component {
     picture: "",
   };
   render() {
+    console.log(process.env.REACT_APP_FACEBOOK_APP_ID);
+    let fbContent;
+    if (this.state.isLoggedIn) {
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
     return <div></div>;
   }
 }
